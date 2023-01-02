236affd6891c6466f2bef1452f85af3ec9d14766
Ompolar <yuda.elvonda@gmail.com>
2023-01-02 20:39:15 +0700

    testing from Ali Akbar

--- app/__test__/user.test.js
+++ app/__test__/user.test.js
@@ -2,178 +2,119 @@ const request = require("supertest");
 const app = require("../index");
 const { User } = require("../models");
 
+//for admin
+//getting list of member
+describe("GET /v1/admin/all", () => {
+  describe("getting all users data", () => {
+    it("should respond with a 200 status code", async () => {
+      const response = await request(app).get("/v1/ticket");
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
 
-describe("GET /v1/ticket", () => {
-    describe("getting all ticket data", () => {
-      test("should respond with a 200 status code", async () => {
-        const response = await request(app).get("/v1/ticket")
-        expect(response.statusCode).toBe(200)
-      })
-    })
-})
+describe("PUT /v1/admin/update/:id", () => {
+  let user;
 
-describe("GET /v1/ticket-doms", () => {
-    describe("getting all ticket data", () => {
-      test("should respond with a 200 status code", async () => {
-        const response = await request(app).get("/v1/ticket-doms")
-        expect(response.statusCode).toBe(200)
-      })
-    })
-})
+  beforeEach(async () => {
+    user = await User.create({
+      name: "Lovelly Super Admin",
+      email: "superadmin@gmail.com",
+      password: "test",
+      image:
+        "https://res.cloudinary.com/dptgh7efj/image/upload/v1668438995/user/k4qprdipa96sl2c3ugd0.jpg",
+      phone: "+62 898 1234 4321",
+      birth: new Date("02/02/2022"),
+      role: "superadmin",
+      isExist: true,
+      isVerify: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    return user;
+  });
 
-describe("GET /v1/ticket-intr", () => {
-    describe("getting all ticket data", () => {
-      test("should respond with a 200 status code", async () => {
-        const response = await request(app).get("/v1/ticket-intr")
-        expect(response.statusCode).toBe(200)
-      })
-    })
-})
+  afterEach(() => user.destroy());
 
-// describe("GET /v1/ticket/:id", () => {
-//   let ticket;
-
-//   beforeEach(async () => {
-//     ticket = await Ticket.create({
-//       code: "DO22TO05AR05000",
-//       flight: "DOMESTIC",
-//       departure: "Bandar Udara Internasional Sultan Iskandar Muda, Banda Aceh",
-//       departureCode: "BTJ",
-//       destination: "Bandar Udara Sultan Aji Muhammad Sulaiman, Balikpapan",
-//       destinationCode: "BPN",
-//       type: "Adult",
-//       class: "Business",
-//       takeOff: new Date(),
-//       arrive: new Date(),
-//       price: 3000000,
-//       totalChair: 100,
-//       createdAt: new Date(),
-//       updatedAt: new Date(),
-//     });
-//     return ticket;
-//   });
-
-//   afterEach(() => ticket.destroy());
-
-//   it("should response with 200 as status code", async () => {
-//     return request(app)
-//       .get("/v1/ticket/" + ticket.id)
-//       .then((res) => {
-//         expect(res.statusCode).toBe(200);
-//       });
-//   });
-// });
-
-
-describe("POST /v1/ticket", () => {
-  it("should response with 201 as status code", async () => {
-    const code = "DO23TO05AR07000";
-    const flight = "DOMESTIC";
-    const departure = "Bandar Udara Internasional Sultan Iskandar Muda, Banda Aceh";
-    const departureCode = "BTJ";
-    const destination = "Bandar Udara Sultan Aji Muhammad Sulaiman, Balikpapan";
-    const destinationCode = "BPN";
-    const type = "Adult";
-    const tclass = "Business";
-    const takeOff = new Date();
-    const arrive = new Date();
-    const price = 3000000;
-    const totalChair = 100;
+  it("should response with 401 as status code", async () => {
+    const name = "Lovelly Super Admin";
+    const email = "superadmin@gmail.com";
+    const password = "test";
+    const image =
+      "https://res.cloudinary.com/dptgh7efj/image/upload/v1668438995/user/k4qprdipa96sl2c3ugd0.jpg";
+    const phone = "+62 898 1234 4321";
+    const birth = new Date("02/02/2022");
+    const role = "superadmin";
+    const isExist = true;
+    const isVerify = true;
     const createdAt = new Date();
     const updatedAt = new Date();
 
     return request(app)
-      .post("/v1/ticket")
+      .put("/v1/admin/update/" + user.id)
       .set("Content-Type", "application/json")
-      .send({ code, flight, departure, departureCode, destination, destinationCode, type, tclass, takeOff, arrive, price, totalChair, createdAt, updatedAt })
+      .send({
+        name,
+        email,
+        password,
+        image,
+        phone,
+        birth,
+        role,
+        isExist,
+        isVerify,
+        createdAt,
+        updatedAt,
+      })
       .then((res) => {
-        expect(res.statusCode).toBe(201);
+        expect(res.statusCode).toBe(401);
+        // expect(res.body).toEqual(
+        //   expect.objectContaining({
+        //     name,
+        //     email,
+        //     password,
+        //     image,
+        //     phone,
+        //     birth,
+        //     role,
+        //     isExist,
+        //     isVerify,
+        //     createdAt,
+        //     updatedAt
+        //   })
+        // )
       });
   });
 });
 
-describe("PUT /v1/ticket/:id", () => {
-  let ticket;
+describe("DELETE /v1/admin/delete/:id", () => {
+  let user;
 
   beforeEach(async () => {
-    ticket = await Ticket.create({
-      code: "DO22TO05AR05000",
-      flight: "DOMESTIC",
-      departure: "Bandar Udara Internasional Sultan Iskandar Muda, Banda Aceh",
-      departureCode: "BTJ",
-      destination: "Bandar Udara Sultan Aji Muhammad Sulaiman, Balikpapan",
-      destinationCode: "BPN",
-      type: "Adult",
-      class: "Business",
-      takeOff: new Date(),
-      arrive: new Date(),
-      price: 3000000,
-      totalChair: 100,
+    user = await User.create({
+      name: "Lovelly Super Admin",
+      email: "superadmin@gmail.com",
+      password: "test",
+      image:
+        "https://res.cloudinary.com/dptgh7efj/image/upload/v1668438995/user/k4qprdipa96sl2c3ugd0.jpg",
+      phone: "+62 898 1234 4321",
+      birth: new Date("02/02/2022"),
+      role: "superadmin",
+      isExist: true,
+      isVerify: true,
       createdAt: new Date(),
       updatedAt: new Date(),
     });
-    return ticket;
+    return user;
   });
 
-  afterEach(() => ticket.destroy());
+  afterEach(() => user.destroy());
 
-  it("should response with 200 as status code", async () => {
-    const code = "DO23TO05AR07000";
-    const flight = "DOMESTIC";
-    const departure = "Bandar Udara Internasional Sultan Iskandar Muda, Banda Aceh";
-    const departureCode = "BTJ";
-    const destination = "Bandar Udara Sultan Aji Muhammad Sulaiman, Balikpapan";
-    const destinationCode = "BPN";
-    const type = "Adult";
-    const tclass = "Business";
-    const takeOff = new Date();
-    const arrive = new Date();
-    const price = 1500000;
-    const totalChair = 100;
-    const createdAt = new Date();
-    const updatedAt = new Date();
-
+  it("should response with 401 as status code", async () => {
     return request(app)
-      .put("/v1/ticket/" + ticket.id)
-      .set("Content-Type", "application/json")
-      .send({ code, flight, departure, departureCode, destination, destinationCode, type, tclass, takeOff, arrive, price, totalChair, createdAt, updatedAt })
+      .delete("/v1/admin/delete/" + user.id)
       .then((res) => {
-        expect(res.statusCode).toBe(200);
+        expect(res.statusCode).toBe(401);
       });
   });
-});
-
-describe("DELETE /v1/ticket/:id", () => {
-  let ticket;
-
-  beforeEach(async () => {
-    ticket = await Ticket.create({
-      code: "DO22TO05AR05000",
-      flight: "DOMESTIC",
-      departure: "Bandar Udara Internasional Sultan Iskandar Muda, Banda Aceh",
-      departureCode: "BTJ",
-      destination: "Bandar Udara Sultan Aji Muhammad Sulaiman, Balikpapan",
-      destinationCode: "BPN",
-      type: "Adult",
-      class: "Business",
-      takeOff: new Date(),
-      arrive: new Date(),
-      price: 3000000,
-      totalChair: 100,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    return ticket;
-  });
-
-  afterEach(() => ticket.destroy());
-
-  it("should response with 204 as status code", async () => {
-    return request(app)
-      .delete("/v1/ticket/" + ticket.id)
-      .then((res) => {
-        expect(res.statusCode).toBe(204);
-      });
-  });
-
 });
